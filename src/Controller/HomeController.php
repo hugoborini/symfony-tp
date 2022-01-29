2ee57bb3e68c98bf95d5fc81a45c765aa0711574
hugoborini <hugo.borini@hetic.net>
2022-01-29 18:03:20 +0100

    {add} add profiler

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render("frontend/Home.html.twig",[
+            
         ]);
 
     }
